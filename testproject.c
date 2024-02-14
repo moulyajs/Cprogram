decc549868b09e13d8cb5f85b281a0d81920ae61
Moulya <moulya.anjanappa@gmail.com>
2024-02-14 22:44:02 +0530

    updated

--- testproject.c
+++ testproject.c
@@ -224,6 +224,7 @@ int main()
     return 0;
 }
 */
+/*
 #include <stdio.h>
 int main()
 {
@@ -238,5 +239,85 @@ int main()
     printf("ram\n lakshman\n");
     printf("ram\t bheem\n");
     printf("\"Mahabharat\"");
+    return 0;
+}
+*/
+// calculate weekpay
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#define PAYRATE 12.0
+#define TAXRATE_150 .20
+#define TAXRATE_300 .15
+#define TAXRATE_REST .25
+#define OVERTIME 40
+
+int main()
+{
+    int hours = 0;
+    double grosspay = 0.0;
+    double taxes = 0.0;
+    double netpay = 0.0;
+    printf("Please enter the number of hours worked this week:\n");
+    scanf("%d", &hours);
+    if (hours <= 40)
+        grosspay = hours * PAYRATE;
+    else
+    {
+        grosspay = 40 * PAYRATE;
+        double overtimepay = (hours - 40) * (PAYRATE * 1.5);
+        grosspay += overtimepay;
+    }
+
+    if (grosspay <= 300)
+    {
+        taxes = grosspay * TAXRATE_300;
+    }
+    else if (grosspay > 300 && grosspay <= 450)
+    {
+        taxes = 300 * TAXRATE_300;
+        taxes += (grosspay - 300) * TAXRATE_150;
+    }
+    else
+    {
+        taxes = 300 * TAXRATE_300;
+        taxes = 150 * TAXRATE_150;
+        taxes += (grosspay - 450) * TAXRATE_REST;
+    }
+
+    netpay = grosspay - taxes;
+
+    printf("Your grosspay this week %.2f\n", grosspay);
+    printf("Your taxes this week %.2f\n", taxes);
+    printf("Your netpay this week %.2f\n", netpay);
+    return 0;
+}
+*/
+#include <stdio.h>
+int main()
+{
+    enum weekday
+    {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+    };
+    enum weekday today = saturday;
+    switch (today)
+    {
+    case sunday:
+        printf("today is sunday");
+        break;
+    case monday:
+        printf("today is monday");
+        break;
+    default:
+        printf("whatever");
+        break;
+    }
     return 0;
 }
