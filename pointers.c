6394ccd3ec45414e574bca9a91d5366831052156
Moulya <moulya.anjanappa@gmail.com>
2024-03-03 18:16:11 +0530

    updated

--- pointers.c
+++ pointers.c
@@ -31,3 +31,109 @@ int main()
     return 0;
 }
 */
+/*
+#include <stdio.h>
+int main(void)
+{
+    long num1 = 0L;
+    long num2 = 0L;
+    long *pnum = NULL;
+
+    pnum = &num1;
+    *pnum = 2L;
+    ++num2;
+    num2 += *pnum;
+
+    pnum = &num2;
+    ++*pnum;
+
+    printf("num1 = %ld  num2 = %ld  *pnum = %ld  *pnum + num2 = %ld  ", num1, num2, *pnum, *pnum + num2);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <string.h>
+
+void swap(int x, int y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+    return;
+}
+int main(void)
+{
+    int a = 100;
+    int b = 200;
+    printf("Before swap, value of a %d\n", a);
+    printf("Before swap, value of b %d\n", b);
+    swap(a, b);
+    printf("After swap, value of a %d\n", a);
+    printf("After swap, value of b %d\n", b);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <string.h>
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    return;
+}
+int main(void)
+{
+    int a = 100;
+    int b = 200;
+    printf("Before swap, value of a %d\n", a);
+    printf("Before swap, value of b %d\n", b);
+    swap(&a, &b);
+    printf("After swap, value of a %d\n", a);
+    printf("After swap, value of b %d\n", b);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+void square(int *const x);
+
+int main()
+{
+    int num = 6;
+    square(&num);
+    printf("the square of the number is %d", num);
+
+    return 0;
+}
+
+void square(int *const x)
+{
+    *x = (*x) * (*x);
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+void square(int *x);
+
+int main()
+{
+    int *num = (int *)malloc(sizeof(int));
+    *num = 4;
+    square(num);
+    printf("the square of the number is %d", *num);
+
+    return 0;
+}
+
+void square(int *x)
+{
+    *x = (*x) * (*x);
+}
+*/
