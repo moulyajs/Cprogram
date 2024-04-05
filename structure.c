477e1cafd424fd4318704ac1796f021374500f56
Moulya <moulya.anjanappa@gmail.com>
2024-04-05 22:37:29 +0530

    updated

---
+++ structure.c
@@ -0,0 +1,28 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+struct employee
+{
+    char name[30];
+    char date[30];
+    float salary;
+};
+int main()
+{
+    struct employee emp = {"mike", "12/3/03", 23456.09};
+    printf("name %s\n", emp.name);
+    printf("hire date %s\n", emp.date);
+    printf("salary %.2f\n", emp.salary);
+    printf("enter the employee's information:");
+    printf("enter name:");
+    scanf("%s", emp.name);
+    printf("enter date:");
+    scanf("%s", emp.date);
+    printf("enter salary:");
+    scanf("%f", &emp.salary);
+    printf("name %s\n", emp.name);
+    printf("hire date %s\n", emp.date);
+    printf("salary %.2f\n", emp.salary);
+    return 0;
+}
+*/
